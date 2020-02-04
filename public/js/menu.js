0377a9535f6eef32acece90ce34c610ee570d6eb
fryn4538 <fredrik.yngve.4538@student.uu.se>
2020-02-04 14:26:28 +0100

    Task 4-9 completed

---
+++ public/js/menu.js
@@ -0,0 +1,43 @@
+
+let food = [
+    {
+	"name": "Snesko burger",
+	"kCal": 999,
+	"lactose": true,
+	"gluten": true,
+	"vegan": false,
+	"img": "https://www.burgerdudes.se/wp-content/uploads/2018/09/shadyburger_basic_burger_stefan_med.jpg"
+    },
+    {
+	"name": "Le Monde burger",
+	"kCal": 1,
+	"lactose": false,
+	"gluten": false,
+	"vegan": true,
+	"img": "https://sweetsimplevegan.com/wp-content/uploads/2019/06/Easy-5-Ingredient-Veggie-Burgers-Sweet-Simple-Vegan-thumbnail.jpg"
+    },
+    {
+	"name": "La Folie Douce burger",
+	"kCal": 500,
+	"lactose": true,
+	"gluten": true,
+	"vegan": false,
+	"img": "https://www.thelastfoodblog.com/wp-content/uploads/2017/04/Halloumi-Burgers-thumb.jpg"
+    },
+    {
+	"name": "The Frog burger",
+	"kCal": 680,
+	"lactose": true,
+	"gluten": false,
+	"vegan": false,
+	"img": "https://assets.biggreenegg.eu/app/uploads/2019/03/28145521/topimage-classic-hamburger-2019m04-800x534.jpg"
+    },
+    {
+	"name": "Summit burger",
+	"kCal": 1500,
+	"lactose": false,
+	"gluten": true,
+	"vegan": true,
+	"img": "https://cdn-rdb.arla.com/Files/arla-se/2409428905/01a6ac3d-4676-4781-8643-5cbf4387d9dd.jpeg?mode=crop&w=479&h=335&ak=f525e733&hm=d7d1b1dd"
+    }
+]
