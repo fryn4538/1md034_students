74dbfbe54603052ed12913b031a7baea1d656f4e
fryn4538 <fredrik.yngve.4538@student.uu.se>
2020-02-10 13:51:56 +0100

    Task 5-2 completed

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -15,7 +15,11 @@ const vm = new Vue({
   }
 })
 */
-const vm = new Vue({
+
+'use strict';
+const socket = io();
+
+const gm = new Vue({
     el: '#wrap',
     data: {
 	food,
@@ -36,11 +40,20 @@ const vuem = new Vue({
 	street:"",
 	num:"",
 	selected:"",
-	picked:""
+	picked:"",
+	orders: {},
+    },
+    created: function() {
+	socket.on('initialize', function(data) {
+	    this.orders = data.orders;
+	}.bind(this));
+	socket.on('currentQueue', function(data) {
+	    this.orders = data.orders;
+	}.bind(this));
     },
     methods: {
-        markDone: function(name, email, street, num, selected, picked) {
-	    let order = [name, email, street, num, selected, picked];
+        markDone: function(name, email, selected, picked) {
+	    let order = [name, email, selected, picked];
 	    let arr = vm.getBurger();
 	    let div = document.getElementById("orderInfo");
 	    for(let i = 0; i < order.length; i++){
@@ -56,6 +69,26 @@ const vuem = new Vue({
 		div.appendChild(foo);
 	    }
 	    
-        }
+        },
+	getNext: function() {
+	    let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+		return Math.max(last, next);
+	    }, 0);
+	    return lastOrder + 1;
+	},
+	addOrder: function(event) {
+	    let offset = {
+		x: event.currentTarget.getBoundingClientRect().left,
+		y: event.currentTarget.getBoundingClientRect().top,
+	    };
+	    socket.emit('addOrder', {
+		orderId: this.getNext(),
+		details: {
+		    x: event.clientX - 10 - offset.x,
+		    y: event.clientY - 10 - offset.y,
+		},
+		orderItems: ['Beans', 'Curry'],
+	    });
+	},
     }
 })
